6a790295a9cb70097db4883fc7cb42417b84dccb
sidarcoban <sidarcoban@gmail.com>
2014-10-10 13:15:28 +0300

    Signed-off-by: sidarcoban <sidarcoban@gmail.com>

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+feature "StaticPages" do
+  feature "GET /static_pages" do
+    it "works! (now write some real specs)" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      page.status_code.should be 200
+    end
+  end
+end
+
+
+feature "About page" do
+  it "should have the content 'About Us'" do
+    visit '/static_pages/about'
+    page.status_code.should be 200
+  end
+end
+
+feature "Home page" do
+  it "should have the h1 'Sample App'" do
+    visit '/static_pages/home'
+    page.should have_title('Sample App')
+  end
+  it "should have the title 'Home'" do
+    visit '/static_pages/home'
+    page.should have_title("Ruby on Rails Tutorial Sample App | Home")
+  end
+end
+feature "Help page" do
+  it "should have the h1 'Help'" do
+    visit '/static_pages/help'
+    page.should have_title('Help')
+  end
+  it "should have the title 'Help'" do
+    visit '/static_pages/help'
+    page.should have_title("Ruby on Rails Tutorial Sample App | Help")
+  end
+end
+feature "About page" do
+  it "should have the h1 'About Us'" do
+    visit '/static_pages/about'
+    page.should have_title('About Us')
+  end
+  it "should have the title 'About Us'" do
+    visit '/static_pages/about'
+    page.should have_title("Ruby on Rails Tutorial Sample App | About Us")
+  end
+end
